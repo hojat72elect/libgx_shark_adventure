cfcd2ac3fdf3a6f9adf19941fcfc6231db405f85
hojat <hojat72elect@gmail.com>
2024-10-16 03:19:36 -0400

    Settings.java -> kt

--- core/src/com/nopalsoft/sharkadventure/Settings.kt
+++ core/src/com/nopalsoft/sharkadventure/Settings.kt
@@ -1,46 +1,51 @@
-package com.nopalsoft.sharkadventure;
+package com.nopalsoft.sharkadventure
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Preferences;
+import com.badlogic.gdx.Gdx
 
-public class Settings {
+object Settings {
 
-    private final static Preferences pref = Gdx.app.getPreferences("com.nopalsoft.sharkadventure");
-    public static long numberOfTimesPlayed = 0;
-    public static long bestScore;
-    public static boolean isMusicOn = true;
-    public static boolean isSoundOn = true;
-    public static boolean didBuyNoAds = false;
-    protected static boolean didRate = false;
+    private val pref = Gdx.app.getPreferences("com.nopalsoft.sharkadventure")
 
-    public static void save() {
-        pref.putBoolean("isMusicOn", isMusicOn);
-        pref.putBoolean("isSoundOn", isSoundOn);
-        pref.putBoolean("didBuyNoAds", didBuyNoAds);
-        pref.putBoolean("didRate", didRate);
-        pref.putLong("numVecesJugadas", numberOfTimesPlayed);
-        pref.putLong("bestScore", bestScore);
-        pref.flush();
+    @JvmField
+    var numberOfTimesPlayed = 0L
+
+    @JvmField
+    var bestScore = 0L
+
+    @JvmField
+    var isMusicOn = true
+
+    @JvmField
+    var isSoundOn = true
+    private var didBuyNoAds = false
+    private var didRate = false
+
+    @JvmStatic
+    fun save() {
+        pref.putBoolean("isMusicOn", isMusicOn)
+        pref.putBoolean("isSoundOn", isSoundOn)
+        pref.putBoolean("didBuyNoAds", didBuyNoAds)
+        pref.putBoolean("didRate", didRate)
+        pref.putLong("numVecesJugadas", numberOfTimesPlayed)
+        pref.putLong("bestScore", bestScore)
+        pref.flush()
     }
 
-    public static void load() {
-        isMusicOn = pref.getBoolean("isMusicOn", true);
-        isSoundOn = pref.getBoolean("isSoundOn", true);
-
-        didBuyNoAds = pref.getBoolean("didBuyNoAds", false);
-        didRate = pref.getBoolean("didRate", false);
-
-        numberOfTimesPlayed = pref.getLong("numVecesJugadas", 0);
-
-        bestScore = pref.getLong("bestScore", 0);
-
+    @JvmStatic
+    fun load() {
+        isMusicOn = pref.getBoolean("isMusicOn", true)
+        isSoundOn = pref.getBoolean("isSoundOn", true)
+        didBuyNoAds = pref.getBoolean("didBuyNoAds", false)
+        didRate = pref.getBoolean("didRate", false)
+        numberOfTimesPlayed = pref.getLong("numVecesJugadas", 0)
+        bestScore = pref.getLong("bestScore", 0)
     }
 
-    public static void setBestScore(long score) {
+    @JvmStatic
+    fun setBestScore(score: Long) {
         if (score > bestScore) {
-            bestScore = score;
-            save();
+            bestScore = score
+            save()
         }
     }
-
 }
