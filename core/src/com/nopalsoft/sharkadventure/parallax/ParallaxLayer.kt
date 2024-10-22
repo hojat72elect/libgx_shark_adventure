d22df781a1470c9dabac7f3417da717722f21fcc
hojat <hojat72elect@gmail.com>
2024-10-22 00:20:49 -0400

    ParallaxLayer.java -> kt

--- core/src/com/nopalsoft/sharkadventure/parallax/ParallaxLayer.kt
+++ core/src/com/nopalsoft/sharkadventure/parallax/ParallaxLayer.kt
@@ -1,27 +1,28 @@
-package com.nopalsoft.sharkadventure.parallax;
+package com.nopalsoft.sharkadventure.parallax
 
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.graphics.g2d.TextureRegion
+import com.badlogic.gdx.math.Vector2
 
-public class ParallaxLayer {
-    public TextureRegion region;
-    public Vector2 parallaxRatio;
-    public Vector2 startPosition;
-    public Vector2 padding;
-    public float width, height;
-
-    /**
-     * @param region        the TextureRegion to draw , this can be any width/height
-     * @param parallaxRatio the relative speed of x,y {@link ParallaxBackground#ParallaxBackground(ParallaxLayer[], float, float, Vector2)}
-     * @param startPosition the init position of x,y
-     * @param padding       the padding of the region at x,y
-     */
-    public ParallaxLayer(TextureRegion region, Vector2 parallaxRatio, Vector2 startPosition, Vector2 padding, float width, float height) {
-        this.region = region;
-        this.parallaxRatio = parallaxRatio;
-        this.startPosition = startPosition;
-        this.padding = padding;
-        this.width = width;
-        this.height = height;
-    }
-}
+/**
+ * this class encapsulates the visual representation, movement behavior, and
+ * initial positioning of a single layer within a larger parallax background.
+ *
+ * @param region The TextureRegion to draw , this can be of any width/height.
+ * @param parallaxRatio The relative speed of x,y {@link ParallaxBackground#ParallaxBackground(ParallaxLayer[], float, float, Vector2)}
+ * @param startPosition The initial position of x,y.
+ * @param padding The padding of the region at x,y.
+ */
+data class ParallaxLayer(
+    @JvmField
+    val region: TextureRegion,
+    @JvmField
+    val parallaxRatio: Vector2,
+    @JvmField
+    val startPosition: Vector2,
+    @JvmField
+    val padding: Vector2,
+    @JvmField
+    val width: Float,
+    @JvmField
+    val height: Float,
+)
